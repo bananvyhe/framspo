53c67750a2555739a8d0dbd8f893485bb4422b78
bananvyhe <loadonden@yahoo.com>
2021-11-08 17:47:24 +0500

    sidekiq downgrade

--- Gemfile
+++ Gemfile
@@ -17,7 +17,7 @@ gem 'webpacker', '~> 5.0'
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.7'
-gem 'sidekiq', '< 7'
+gem 'sidekiq', '< 6'
 gem 'whenever', require: false
 gem 'redis-namespace'
 gem 'sidekiq-client-cli'
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,6 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.16.0"
+set :rbenv_ruby, '3.0.2'
 # namespace :sidekiq do
 #   task :quiet do
 #     on roles(:app) do
@@ -49,45 +50,62 @@ after 'deploy:starting', 'deploy:update_crontab'
 SSHKit.config.command_map[:sidekiq] = "bundle exec sidekiq"
 SSHKit.config.command_map[:sidekiqctl] = "bundle exec sidekiqctl"
 
- set :init_system, :systemd
-set :sidekiq_systemd_unit_name, "sidekiq"
+ # set :init_system, :systemd
+# set :sidekiq_systemd_unit_name, "sidekiq"
+# namespace :sidekiq do
+#   desc 'Quiet sidekiq (stop fetching new tasks from Redis)'
+#   task :quiet do
+#     on roles fetch(:sidekiq_roles) do
+#       # See: https://github.com/mperham/sidekiq/wiki/Signals#tstp
+#       execute :systemctl, '--user', 'kill', '-s', 'SIGTSTP', fetch(:sidekiq_systemd_unit_name), raise_on_non_zero_exit: false
+#     end
+#   end
+
+#   desc 'Stop sidekiq (graceful shutdown within timeout, put unfinished tasks back to Redis)'
+#   task :stop do
+#     on roles fetch(:sidekiq_roles) do
+#       # See: https://github.com/mperham/sidekiq/wiki/Signals#tstp
+#       execute :systemctl, '--user', 'kill', '-s', 'SIGTERM', fetch(:sidekiq_systemd_unit_name), raise_on_non_zero_exit: false
+#     end
+#   end
+
+#   desc 'Start sidekiq'
+#   task :start do
+#     on roles fetch(:sidekiq_roles) do
+#       execute :systemctl, '--user', 'start', fetch(:sidekiq_systemd_unit_name)
+#     end
+#   end
+
+#   desc 'Restart sidekiq'
+#   task :restart do
+#     on roles fetch(:sidekiq_roles) do
+#       execute :systemctl, '--user', 'restart', fetch(:sidekiq_systemd_unit_name)
+#     end
+#   end
+# end
+
 namespace :sidekiq do
-  desc 'Quiet sidekiq (stop fetching new tasks from Redis)'
   task :quiet do
-    on roles fetch(:sidekiq_roles) do
-      # See: https://github.com/mperham/sidekiq/wiki/Signals#tstp
-      execute :systemctl, '--user', 'kill', '-s', 'SIGTSTP', fetch(:sidekiq_systemd_unit_name), raise_on_non_zero_exit: false
+    on roles(:app) do
+      puts capture("pgrep -f 'sidekiq' | xargs kill -TSTP") 
     end
   end
-
-  desc 'Stop sidekiq (graceful shutdown within timeout, put unfinished tasks back to Redis)'
-  task :stop do
-    on roles fetch(:sidekiq_roles) do
-      # See: https://github.com/mperham/sidekiq/wiki/Signals#tstp
-      execute :systemctl, '--user', 'kill', '-s', 'SIGTERM', fetch(:sidekiq_systemd_unit_name), raise_on_non_zero_exit: false
-    end
-  end
-
-  desc 'Start sidekiq'
-  task :start do
-    on roles fetch(:sidekiq_roles) do
-      execute :systemctl, '--user', 'start', fetch(:sidekiq_systemd_unit_name)
-    end
-  end
-
-  desc 'Restart sidekiq'
   task :restart do
-    on roles fetch(:sidekiq_roles) do
-      execute :systemctl, '--user', 'restart', fetch(:sidekiq_systemd_unit_name)
+    on roles(:app) do
+      execute :sudo,  :restart, :workers
     end
   end
 end
-
 after 'deploy:starting', 'sidekiq:quiet'
-after 'deploy:updated', 'sidekiq:stop'
-after 'deploy:published', 'sidekiq:start'
+after 'deploy:reverted', 'sidekiq:restart'
+after 'deploy:published', 'sidekiq:restart'
+
+
+# after 'deploy:starting', 'sidekiq:quiet'
+# after 'deploy:updated', 'sidekiq:stop'
+# after 'deploy:published', 'sidekiq:start'
 after 'deploy:published', 'passenger:restart'
-after 'deploy:failed', 'sidekiq:restart'
+# after 'deploy:failed', 'sidekiq:restart'
 
 # Default value for :pty is false
 # set :pty, true
