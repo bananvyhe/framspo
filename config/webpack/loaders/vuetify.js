b5f3e08681556299c8feef8810865302b6075b79
bananvyhe <loadonden@yahoo.com>
2021-11-12 23:29:28 +0500

    env setup

--- config/webpack/loaders/vuetify.js
+++ config/webpack/loaders/vuetify.js
@@ -1,11 +1,11 @@
+const { dev_server: devServer } = require('@rails/webpacker').config
 
+const isProduction = process.env.NODE_ENV === 'production'
+const inDevServer = process.argv.find(v => v.includes('webpack-dev-server'))
+const extractCSS = !(inDevServer && (devServer && devServer.hmr)) || isProduction
 module.exports = {
-   
-     
       // SASS has different line endings than SCSS
       // and cannot use semicolons in the markup
-      
- 
       // SCSS has different line endings than SASS
       // and needs a semicolon after the import.
        
