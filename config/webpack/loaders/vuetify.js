d5f3abbffcac83976fcf72b32eb627dac0a2fdb1
bananvyhe <loadonden@yahoo.com>
2021-11-13 16:11:36 +0500

    webp setup

--- config/webpack/loaders/vuetify.js
+++ config/webpack/loaders/vuetify.js
@@ -8,24 +8,18 @@ module.exports = {
       // and cannot use semicolons in the markup
       // SCSS has different line endings than SASS
       // and needs a semicolon after the import.
-       
-        test: /\.scss$/,
-        use: [
-          'vue-style-loader',
-             
-          {
-            loader: 'sass-loader',
-     
-            // Requires sass-loader@^8.0.0
-            options: {
-
-              // This is the path to your variables
-                // additionalData: "@import '@/app/javascript/stylesheets/scss/_variables.scss';"
-            },
-       
-          },
-        ],
-       
-     
+  test: /\.scss$/,
+  use: [
+    'vue-style-loader',
+    'css-loader', 
+    {
+      loader: 'sass-loader',
+      // Requires sass-loader@^8.0.0
+      options: {
+        // This is the path to your variables
+        // additionalData: "@import '@/app/javascript/stylesheets/scss/_variables.scss';"
+      },
    
+    },
+  ],  
 }
