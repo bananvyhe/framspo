c60e8fc5428fba1023bd74a5e304801de7e22f90
bananvyhe <loadonden@yahoo.com>
2021-11-13 16:27:28 +0500

    node-sass install

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,12 +2,14 @@ const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
 const vuetify = require('./loaders/vuetify')
+
 const sassLoader = environment.loaders.get('sass')
 const sassLoaderConfig = sassLoader.use.find(function(element) {
   return element.loader == 'sass-loader'
 })
 const options = sassLoaderConfig.options
 options.implementation = require('sass')
+
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 environment.loaders.prepend('vuetify', vuetify)
