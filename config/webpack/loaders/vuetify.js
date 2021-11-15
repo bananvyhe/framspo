19dfd241181767636eb618130e0999331d59fdd1
bananvyhe <loadonden@yahoo.com>
2021-11-15 08:25:25 +0500

    change node-sass to dart-sass

--- config/webpack/loaders/vuetify.js
+++ config/webpack/loaders/vuetify.js
@@ -1,20 +1,39 @@
+
 module.exports = {
       // SASS has different line endings than SCSS
       // and cannot use semicolons in the markup
       // SCSS has different line endings than SASS
       // and needs a semicolon after the import.
-  test: /\.scss$/,
-  use: [
-    'vue-style-loader',
-    'css-loader', 
+  rules: [
+     // SASS has different line endings than SCSS
+      // and cannot use semicolons in the markup
+      // {
+      //   test: /\.sass$/,
+      //   use: [
+      //     'vue-style-loader',
+      //     'css-loader',
+      //     {
+      //       loader: 'sass-loader',
+      //       options: {
+      //       },
+      //     },
+      //   ],
+      // },
     {
-      loader: 'sass-loader',
-      // Requires sass-loader@^8.0.0
-      options: {
-        // This is the path to your variables
-        // additionalData: "@import '@/app/javascript/stylesheets/scss/_variables.scss';"
-      },
-   
+      test: /\.scss$/,
+      use: [
+        'vue-style-loader',
+        'css-loader', 
+        {
+          loader: 'sass-loader',
+        // Requires sass-loader@^8.0.0
+          options: {
+          // This is the path to your variables
+          // additionalData: "@import '@/app/javascript/stylesheets/scss/_variables.scss';"
+          },
+        },
+      ],  
     },
-  ],      
+  ],
+    
 }
