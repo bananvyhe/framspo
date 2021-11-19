cef7f64b0816f0cd640f299d0cb8102421c5f1c2
bananvyhe <loadonden@yahoo.com>
2021-11-19 17:20:09 +0500

    puts data in array

--- app/workers/hard_worker.rb
+++ app/workers/hard_worker.rb
@@ -6,7 +6,11 @@ class HardWorker < ApplicationController
 	require 'httparty'
 	# require 'openssl'
 	# require 'uri'
+	
 	def perform
+		@rowsd = Array.new
+		@tokenrapid = "bearer " + %x{yc iam create-token}
+		@s = 3
 		def selection_scrapped(row)
 			pic = row.css('img').attr('src').to_s
 			link = row.css('a').attr('href').to_s
@@ -14,69 +18,51 @@ class HardWorker < ApplicationController
 			date = row.css('.date').inner_text.to_s
 			desc = row.css('h2').inner_text.to_s
 
-			tokenrapid = "bearer " + %x{yc iam create-token}
-			@headersb = {
-        "Content-Type" => "application/json",
-        "Authorization" => tokenrapid 
-      }
-      bodyb = {
-        "folderId"=>"b1g86cba4bfmnhhsnobp",
-        "texts"=> head,
-        "targetLanguageCode"=>"ru"
-      }
-      resp = HTTParty.post("https://translate.api.cloud.yandex.net/translate/v2/translate", headers: @headersb, body: bodyb.to_json)
-      render = JSON.parse resp.to_s 
-      puts render
+			@s = @s + 2
+			puts @s
+			data = {
+				:pic => pic,
+				:link => link,
+				:head => head,
+				:date => date,
+				:desc => desc				
+			}
+			@rowsd = [*@rowsd, data]
 
-
-			# rowd = row.css('.item item-list').inner_text
-			# puts '5'
-			# puts rowd
-			# title  = row.css
-			# print title.force_encoding('UTF-8')
-			# File.write('667.html', title.body)
+			# def transclucate |in|
+			# 	@headersb = {
+	  #       "Content-Type" => "application/json",
+	  #       "Authorization" => @tokenrapid 
+	  #     }
+	  #     bodyb = {
+	  #       "folderId"=>"b1g86cba4bfmnhhsnobp",
+	  #       "texts"=> in,
+	  #       "targetLanguageCode"=>"ru"
+	  #     }
+	  #     resp = HTTParty.post("https://translate.api.cloud.yandex.net/translate/v2/translate", headers: @headersb, body: bodyb.to_json)
+	  #     render = JSON.parse resp.to_s 
+	  #     out = render["translations"]
+	  #     tex = out[0]
+	  #     headfin = tex.slice("text")['text']
+	  #     puts headfin
+			# end
 		end
-
-		# puts signin_page.css('.module.latest.news').inner_text
  		agent = Mechanize.new
  		url='https://www.pocketgamer.biz/asia/news/'
  		page = agent.get(url)
- 		# show_more = agent.page
- 		# print show_more.body.force_encoding('UTF-8')
-		# File.write('666.html', show_more.body)
 		@rowsd = Array.new
-		# @n = 0
-		# puts = page_more.link_with(text: 'More').click
-		# puts page.css('.module.latest.news').inner_text
-
 		page.css('.module.latest.news').each do |row|
-			# puts row.css('.item.item-large').attr('href').to_s
-
-			# @n = @n + 1 
-			# puts @n
-			# if @n == 7
-				@m = 0
-				rowcss = row.css('.item')
-				rowcss.each do |rowf|
-				@m = @m + 1
-				puts @m
+			@m = 0
+			rowcss = row.css('.item')
+			rowcss.each do |rowf|
+			@m = @m + 1
+			# puts @m
 				if @m != 5
 					selection_scrapped(rowf)
-					# puts rowf.css('img').attr('src').to_s
-					# puts rowf.css('a').attr('href').to_s
-					# puts rowf.css('.qualifier').inner_text.to_s
-					# puts rowf.css('.date').inner_text.to_s
-					# puts rowf.css('h2').inner_text.to_s
 				end
- 				# selection_scrapped(rowf)
- 				# puts "ds"
- 				end
-			# end
- 			# 	rowd = row.css('.container').inner_text 
- 			# 	puts rowd
-			# @rowsd = [*@rowsd, rowd]
+			end
 		end
-		# File.write('668.html', @rowsd)
+		puts @rowsd
 	end
- 
+
 end
