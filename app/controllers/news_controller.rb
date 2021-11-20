1b9d14d64e5a1b4b76f90a183a09b2a08529591c
bananvyhe <loadonden@yahoo.com>
2021-11-21 00:34:31 +0500

    generate token another variant

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,16 +1,36 @@
 class NewsController < ApplicationController
 	skip_before_action :verify_authenticity_token
-
+	require 'net/http'
+	require 'uri'
   def index
   	@news = News.all 
 		render json: @news.order(created_at: :desc)
   end
 
 	def create 
-		ycc =  `yc iam create-token`
-		# print ycc
-		tokenrapid = "bearer " + ycc
-		print tokenrapid
+		# curl -d "{\"yandexPassportOauthToken\":\"Rails.application.credentials.yaOauth\"}" "https://iam.api.cloud.yandex.net/iam/v1/tokens"
+
+		uri = URI.parse("https://iam.api.cloud.yandex.net/iam/v1/tokens")
+		request = Net::HTTP::Post.new(uri)
+
+		request.body = JSON.dump({
+		  "yandexPassportOauthToken" => Rails.application.credentials.yaOauth
+		})
+		req_options = {
+		  use_ssl: uri.scheme == "https",
+		}
+		response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+		  @tokwars =  http.request(request).body
+		end
+	# response.code
+	# response.body
+		formj = JSON.parse(@tokwars)
+		tokenrapid = "bearer " + formj['iamToken']
+
+	# ycc =  `yc iam create-token`
+	# print ycc
+	# tokenrapid = "bearer " + ycc
+	# print tokenrapid
 		params.require(:_json).each do |d|
     	pic = d[:pic].to_s
       head = d[:head].to_s
--- app/workers/tobd_worker.rb
+++ app/workers/tobd_worker.rb
@@ -4,14 +4,14 @@ class TobdWorker < ApplicationController
   require 'httparty' 
   def perform (pic, head, desc, date, link, tokenrapid)
 
-    # puts desc
     @n = 0
     def tranklukate(ina, tokenrapid)
+      # puts tokenrapid
       @n = @n + 1
       puts @n.to_s + "tranklucating.."
         @headersb = {
           "Content-Type" => "application/json",
-          "Authorization" => tokenrapid 
+          "Authorization" => tokenrapid
         }
 
         bodyb = {
@@ -19,51 +19,20 @@ class TobdWorker < ApplicationController
           "texts"=> ina,
           "targetLanguageCode"=>"ru"
         }
-
+         
         resp = HTTParty.post("https://translate.api.cloud.yandex.net/translate/v2/translate", headers: @headersb, body: bodyb.to_json)
         render = JSON.parse resp.to_s 
         out = render["translations"]
         tex = out[0]
         headfin = tex.slice("text")['text']
-        # puts headfin
     end
 
-    headfin = tranklukate(head, tokenrapid).to_s
-    contentfin = tranklukate(desc, tokenrapid).to_s
-
+    headfin = tranklukate(head, tokenrapid)
     sleep(1)
+    contentfin = tranklukate(desc, tokenrapid)
+
     @news = News.new({:pic => pic, :head => headfin,:desc => contentfin,:date => date,:link => link})
     @news.save
-
-    # puts headfin
-
-
-     # @headersb = {
-     #    "Content-Type" => "application/json",
-     #    "Authorization" => tokenrapid 
-     #  }
-
-   #  def tranklukator(zap) 
-   #    puts "tranklufication..."
-   #    bodyb = {
-   #      "folderId"=>"b1g86cba4bfmnhhsnobp",
-   #      "texts"=> zap,
-   #      "targetLanguageCode"=>"ru"
-   #    }
-   #    resp = HTTParty.post("https://translate.api.cloud.yandex.net/translate/v2/translate", headers: @headersb, body: bodyb.to_json)
-   #    render = JSON.parse resp.to_s 
-   #    out = render["translations"]
-   #    tex = out[0]
-   #    headfin = tex.slice("text")['text']
-   #  end
-
-   #  #separate translated phrase from responce
-   #  headfin = tranklukator head
-   #  contentfin = tranklukator content
-   #  sleep(1)
-
-  	# @news = News.new({:imglink => imglink, :head => headfin,:content => contentfin,:datepost => datepost,:link => link})
-  	# @news.save
  
 	end
 end
