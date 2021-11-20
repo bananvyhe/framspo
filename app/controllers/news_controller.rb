401fdeb44b2f4b1a5ff5d668a995ae7936afeb93
bananvyhe <loadonden@yahoo.com>
2021-11-20 15:20:58 +0500

    2

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,5 +1,6 @@
 class NewsController < ApplicationController
 	skip_before_action :verify_authenticity_token
+
   def index
   	@news = News.all 
 		render json: @news.order(created_at: :desc)
@@ -7,10 +8,8 @@ class NewsController < ApplicationController
 
 	def create 
 		tokenrapid = "bearer " + %x{yc iam create-token}
-		# puts tokenrapid
-		puts "23452"
-		params.require(:_json).each do |d|
 
+		params.require(:_json).each do |d|
     	pic = d[:pic].to_s
       head = d[:head].to_s
       desc = d[:desc].to_s
@@ -19,33 +18,12 @@ class NewsController < ApplicationController
 
     	unless News.find_by(link: link)  
       	TobdWorker.perform_async(pic, head, desc, date, link, tokenrapid)
-      	puts 'send'
 			end
-			# head = d[:head].to_s
-			# puts head
-			# puts tokenrapid
 		end
-		# params.require(:_json).each do |d|
-
-  #   	pic = d[:pic].to_s
-  #     head = d[:head].to_s
-  #     desc = d[:desc].to_s
-  #     date = d[:date].to_s
-  #     link = d[:link].to_s
-
-  #   	unless News.find_by(link: link)  
-  #     	TobdWorker.perform_async(pic, head, desc, date, link, @tokenrapid)
-  #     	puts 'send'
-		# 	end
-
-		# end 
 	end 
 
 	private
 	  def news_params
     params.require(:news).permit(:pic, :link, :head, :date, :desc)
   end
-
-
-
 end
