ea483fe92cece45fe4728eae2120c199cf579ab9
bananvyhe <loadonden@yahoo.com>
2021-11-23 20:15:02 +0500

    refactoring

--- app/workers/tobd_worker.rb
+++ app/workers/tobd_worker.rb
@@ -4,35 +4,11 @@ class TobdWorker < ApplicationController
   require 'httparty' 
   def perform (pic, head, desc, date, link, tokenrapid)
 
-    @n = 0
-    def tranklukate(ina, tokenrapid)
-      # puts tokenrapid
-      @n = @n + 1
-      puts @n.to_s + "tranklucating.."
-        @headersb = {
-          "Content-Type" => "application/json",
-          "Authorization" => tokenrapid
-        }
-
-        bodyb = {
-          "folderId"=>"b1g86cba4bfmnhhsnobp",
-          "texts"=> ina,
-          "targetLanguageCode"=>"ru"
-        }
-         
-        resp = HTTParty.post("https://translate.api.cloud.yandex.net/translate/v2/translate", headers: @headersb, body: bodyb.to_json)
-        render = JSON.parse resp.to_s 
-        out = render["translations"]
-        tex = out[0]
-        headfin = tex.slice("text")['text']
-    end
-
-    headfin = tranklukate(head, tokenrapid)
+    headfin = News.tranklukate(head, tokenrapid)
     sleep(1)
-    contentfin = tranklukate(desc, tokenrapid)
+    contentfin = News.tranklukate(desc, tokenrapid)
 
     @news = News.new({:pic => pic, :head => headfin,:desc => contentfin,:date => date,:link => link})
     @news.save
- 
 	end
 end
