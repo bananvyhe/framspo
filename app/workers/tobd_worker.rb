c287558c29d587f2daf7447f185c77182c734ac2
bananvyhe <loadonden@yahoo.com>
2022-07-22 00:23:10 +0500

    1

--- app/workers/tobd_worker.rb
+++ app/workers/tobd_worker.rb
@@ -3,11 +3,11 @@ class TobdWorker < ApplicationController
   require 'json'
   require 'httparty' 
   def perform (pic, head, desc, date, link, tokenrapid)
-
-    headfin = News.tranklukate(head, tokenrapid)
-    sleep(1)
-    contentfin = News.tranklukate(desc, tokenrapid)
     unless News.find_by(link: link)  
+      headfin = News.tranklukate(head, tokenrapid)
+      sleep(1)
+      contentfin = News.tranklukate(desc, tokenrapid)
+     
       @news = News.new({:pic => pic, :head => headfin,:desc => contentfin,:date => date,:link => link})
       @news.save
     end
