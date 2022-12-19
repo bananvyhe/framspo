c8b6060bab629c48e23208202d484f00ace1b48d
bananvyhe <loadonden@yahoo.com>
2022-12-20 00:58:32 +0500

    drop fix

--- app/javascript/packs/components/drop.vue
+++ app/javascript/packs/components/drop.vue
@@ -2,16 +2,16 @@
  <div class="dropzone d-flex "> 
   <!-- {{drop}} -->
   <!-- {{pumpkdead}} -->
-      <div class="d-flex justify-center drop">
-
-        <div v-for="(item, index) in drop">
+      <div class="d-flex drop ">
+<!-- {{pumpkdead}} -->
+        <div v-for="(item, index) in drop" :key = "item.id" >
             <!-- {{item.id}} -->
 
           <v-tooltip top >
-            <template   v-slot:activator="{ on, attrs}" :name="''+item.id" class="d-flex justify-center">
+            <template   v-slot:activator="{ on, attrs}" :name="''+item.id" class="d-flex justify-end">
 
-              <div :style="[ signedIn == true ?  {cursor: 'pointer'}:{cursor: 'not-allowed'} ]" v-on:click="pickdrop(item.id)" ><div  v-bind="attrs" v-on="on" class="ore"  v-bind:style="{backgroundImage: 'url(/images/'+item.item+'.png'}"></div></div>
-              <div  class="energy"></div>
+              <div :style="[ signedIn == true ?  {cursor: 'pointer'}:{cursor: 'not-allowed'} ]" v-on:click="pickdrop(item.id)" ><div  v-bind="attrs" v-on="on" class="ore "  v-bind:style="{backgroundImage: 'url(/images/'+item.item+'.png'}"></div></div>
+              <div class="energy "></div>
             </template>
             <span>
               <span style="color:#ffe79f;" >
@@ -57,78 +57,45 @@ export default {
       console.log("drop incoming")
 
 
-   
     },
+
     pumpkdead: function (val){
       console.log("DROPval")
 
     // console.log(val)
-    if (val == true){
-      this.getdrop()
-      console.log('val')
-      console.log(val)
-      console.log('val')
- 
-      setTimeout(function(){
-        var m4 = gsap.timeline();
-          m4.to(".energy",{
-            stagger: 1,
-            delay: 1.9,
-            opacity: 1,
-            // display: "inline"
-            visibility: "visible"
+      if (val == true){
+        this.getdrop()
+        console.log('val')
+        console.log(val)
+        console.log('val')
+   
+      // setTimeout(function(){
 
-          })
-          .to(".energy",{
-            delay:0.7,
-            opacity: 0,
-            // display: "none",
-            visibility: "hidden"
-          })
-          gsap.set(".ore", {
-            y: +15,
-            opacity: 0,
-            // display: "none",
-            visibility: "hidden"
-          });
-          var m3 = gsap.timeline();
-          m3.to(".ore",{
-            stagger: 1,  
-            delay: 1.9,
-            y: 0,
-            opacity: 1,
-            // display: "inline",
-            visibility: "visible",
-            duration: 1.5,
-            ease: "power4.out",
-            onComplete: oreswing
-          })
-            function oreswing(){
-              var m8 = gsap.timeline({repeat: -1});
-              m8.to(".ore",{
-                stagger: 1,
-                ease: "expo.in",
-                y: -2,
-                duration: 3,
-                ease: "elastic.in",
-              })
-              .to(".ore",{
-                // stagger: 1,
-                ease: "elastic.out", 
-                y: 0,
-                duration: 3,
-              })                   
-            }
   
-        },1000 );
+        // },1000 );
       }else{
-        var m3 = gsap.timeline();
-        m3.to(".ore",{
-          opacity: 0,
-          // display: "none",
-          visibility: "hidden",
-          duration: 1
-        })   
+
+            var m8 = gsap.timeline();
+             m8.to(".ore",{
+              
+              opacity: 0,
+             })
+             .to(".ore",{
+              y: 45,
+              display: "none",
+             })
+           
+        // var m7 = gsap.timeline();
+        // m7.to(".ore",{
+        //   opacity: 0,
+ 
+        //   duration: 1
+        // })
+        // .to(".ore",{
+ 
+        //   display: "none",
+        // }) 
+
       }
     }
   },
@@ -151,7 +118,7 @@ export default {
     ...mapState(useLogStore, {
       signedIn: "thissignedIn",
     }),      
-  },    
+  },
   created () {
     console.log("drop")
   },
@@ -159,15 +126,104 @@ export default {
     
   },
   methods: {
+    dropanim(){
+      this.$nextTick(function () {
+
+        var m4 = gsap.timeline();
+        m4.to(".energy",{
+          // stagger: 1.9,
+          stagger: {
+            
+            each: 1.9,
+            // onStart: bgadd,
+            // duration: 0.7,
+            onComplete: bgnull,
+          },
+
+          duration: 1.2,
+          delay: 2.5,
+          opacity: 1,
+          // display: "inline"
+          visibility: "visible",
+          background: 'url(/images/energyonce.gif)',
+          // onComplete: bgnull
+        })
+        // function bgadd(){
+        //   var m11 = gsap.timeline();
+        //    m11.to(".energy",{
+        //     background: 'url(/images/energyonce.gif)',
+        //    })
+        // }
+
+        // var self = this
+        function bgnull(){
+           
+          // console.log(self.pumpkdead)
+          console.log(" pumpkdead")
+          var m9 = gsap.timeline();
+          m9.to(".energy",{
+          // top: 20,
+          background: 'none',
+          // delay: 0.7,
+          // background: 'url(/images/energytwo.gif)',
+          // opacity: 0,
+          // display: "none",
+          // visibility: "hidden"
+
+          })
+        }
+
+        gsap.set(".ore", {
+          y: 45,
+          opacity: 0,
+          display: "none",
+          // visibility: "hidden"
+        });
+
+        var m3 = gsap.timeline();
+        m3.to(".ore",{
+          stagger: 1.2,
+          delay: 2.6,
+          y: 0,
+          opacity: 1,
+          display: "block",
+          visibility: "visible",
+          duration: 1,
+          ease: "power4.out",
+          onComplete: oreswing
+        })
+
+        // var self = this 
+        function oreswing(){
+          var self = this
+          
+          if (self.pumpkdead == true){
+            var m8 = gsap.timeline({repeat: -1});
+            m8.to(".ore",{
+              // stagger: 1,
+              ease: "expo.in",
+              y: -3,
+              duration: 3,
+              ease: "elastic.in",
+            })
+            .to(".ore",{
+              // stagger: 1,
+              ease: "elastic.out", 
+              y: 0,
+              duration: 3,
+            })   
+          }
+
+                
+        }
+      })
+    },    
     pickdrop(val){
       console.log("pickdrop")
        this.$http.secured.post('/my_items/pickdrop',{id: val})
       .then(response => { 
         console.log(response.data)
- 
-
         // this.loareg = response.data
-        
       })
       .catch(error => { this.setError(error, 'Something went wrong') })      
     },
@@ -176,9 +232,8 @@ export default {
       .then(response => { 
         console.log(response.data)
         this.drop = response.data
-
+        this.dropanim()
         // this.loareg = response.data
-        
       })
       .catch(error => { this.setError(error, 'Something went wrong') })
     },   
@@ -188,6 +243,7 @@ export default {
 
 <style lang="css">
 .dropzone{
+  /*overflow: hidden;*/
   position: absolute;
 /*background-color: #da4;*/
 }
@@ -211,7 +267,7 @@ export default {
   /*position: relative;*/
   width: 150px;
   height: 70px;
-  background: url(../../../javascript/images/energy27.gif);
+  /*background: url(../../../javascript/images/energy27.gif);*/
 }
 /*.ore{
   display: none;
@@ -226,13 +282,14 @@ export default {
   cursor: pointer;
 }*/
 .ore{
+
   /*display: none;*/
-  visibility: hidden;
-  opacity: 0;
+  /*visibility: hidden;*/
+  /*opacity: 0;*/
   z-index: 1;
   /*position: absolute;*/
   position: relative;
-  bottom: 0px;
+  /*bottom: 0px;*/
   width: 38px;
   height: 38px;
   /*background: url(../../../javascript/images/goldenore.png);*/
